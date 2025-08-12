4123d254f886ba4f246c5d5d62fec9dfd52cf91f
rmahjabin <157666365+rmahjabin@users.noreply.github.com>
2025-08-12 09:08:54 -0500

    Update unit_conversion.py

--- unit_conversion.py
+++ unit_conversion.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from input_output import read_conversion_constants
 
-def build_conversion_matrix(year):
-  filename = "./data/solar_module_data.csv"
+# def build_conversion_matrix(year): filename = "./data/solar_module_data.csv"
+
+def build_conversion_matrix(year, data_path="./data/solar_module_data.csv"):
+  filename = data_path
   variables = read_conversion_constants(filename, year)
   Wp_per_m2 = 1000
   # Conversion constants for poly-Si
@@ -59,4 +61,3 @@ def convert_dataframe_units(df, target_unit, conversion_matrix):
     return row
 
   return df_converted.apply(convert_row, axis=1)
-
